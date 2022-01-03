61c1dadbdaec535fc5666057afbec155c716fe0a
triono <triono.triono1@gmail.com>
2022-01-03 17:01:34 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('signin', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'registrasi']);
+
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    Route::get('getData', [AuthController::class, 'fetch']);
+    Route::put('updateProfile/{id}/update', [AuthController::class, 'updateProfile']);
+    Route::delete('destroy/{id}/delete', [AuthController::class, 'destroy']);
+
+});
