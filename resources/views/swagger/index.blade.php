e70d6d9bc356b3c1b79e5e99dbe314657e624812
triono <triono.triono1@gmail.com>
2022-01-04 07:17:21 +0700

    changes views/swagger/inde.blade.php

--- resources/views/swagger/index.blade.php
+++ resources/views/swagger/index.blade.php
@@ -1,15 +1,15 @@
 <html>
 <head>
     <title>{{ config('app.name') }} | Frontend API's Swagger</title>
-    <link href="{{asset('swagger/style.css')}}" rel="stylesheet">
+    <link href="https://be-laravel.herokuapp.com/swagger/style.css" rel="stylesheet">
 </head>
 <body>
 <div id="swagger-ui"></div>
-<script src="{{asset('swagger/jquery-2.1.4.min.js')}}"></script>
-<script src="{{asset('swagger/swagger-bundle.js')}}"></script>
+<script src="https://be-laravel.herokuapp.com/swagger/jquery-2.1.4.min.js"></script>
+<script src="https://be-laravel.herokuapp.com/swagger/swagger-bundle.js"></script>
 <script type="application/javascript">
     const ui = SwaggerUIBundle({
-        url: "{{ asset('swagger/swagger.yaml') }}",
+        url: "https://be-laravel.herokuapp.com/swagger/swagger.yaml",
         dom_id: '#swagger-ui',
     });
 </script>
